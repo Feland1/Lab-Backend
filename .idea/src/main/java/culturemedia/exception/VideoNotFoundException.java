8531e566f663e1433bf0ab9b2960b14e53f6d225
Andres Felipe Carvajal Lopez <andres.carvajal5@udea.edu.co>
2024-10-15 22:40:51 -0500

    adding interface and implementation

--- .idea/src/main/java/culturemedia/exception/VideoNotFoundException.java
+++ .idea/src/main/java/culturemedia/exception/VideoNotFoundException.java
@@ -1,12 +1,7 @@
 package culturemedia.exception;
 
-public class VideoNotFoundException extends CultureMediaException {
-
-    public VideoNotFoundException() {
-        super("Video no encontrado");
-    }
-
-    public VideoNotFoundException(String titulo) {
-        super("Video no encontrado: " + titulo);
+public class VideoNotFoundException extends RuntimeException {
+    public VideoNotFoundException(String message) {
+        super(message);
     }
 }
