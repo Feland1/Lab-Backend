eec4b003e15e69ae3983938865f7329cb03032c5
Andres Felipe Carvajal Lopez <andres.carvajal5@udea.edu.co>
2024-10-30 19:15:59 -0500

    fixing error in class after Code Review

--- src/main/java/culturemedia/controllers/CultureMediaController.java
+++ src/main/java/culturemedia/controllers/CultureMediaController.java
@@ -1,26 +1,23 @@
-package culturoteca.controllers;
+package culturemedia.controllers;
 
-import java.util.*;
-
-import culturoteca.exception.VideoNotFoundException;
-import culturoteca.model.Video;
-import culturoteca.service.impl.CultureMediaServiceImpl;
+import java.util.List;
+import culturemedia.exception.VideoNotFoundException;
+import culturemedia.model.Video;
+import culturemedia.impl.CulturemediaServiceImpl;
 
 public class CultureMediaController {
 
-    private final CultureMediaServiceImpl cultureMediaService;
+    private final CulturemediaServiceImpl cultureMediaService;
 
-
-    public CultureMediaController(CultureMediaServiceImpl cultureMediaService) {
+    public CultureMediaController(CulturemediaServiceImpl cultureMediaService) {
         this.cultureMediaService = cultureMediaService;
     }
 
-
-    public List<Video> find_allVideos() throws VideoNotFoundException {
-        List<Video> videos = null;
-        videos = cultureMediaService.findAll();
+    public List<Video> findAllVideos() throws VideoNotFoundException {
+        List<Video> videos = cultureMediaService.findAll();
+        if (videos.isEmpty()) {
+            throw new VideoNotFoundException("No videos found.");
+        }
         return videos;
     }
-
-
 }
